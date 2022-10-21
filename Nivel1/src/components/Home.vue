62d54d2e1cc81ab5fcf18acef0d2495c91345f82
aobjsvue <andres.orta@hotmail.com>
2022-10-21 15:54:53 +0200

    exercise2

--- Nivel1/src/components/Home.vue
+++ Nivel1/src/components/Home.vue
@@ -6,6 +6,7 @@
                 <input type="checkbox" :value="500" v-model="prices" @change="updateTotal" />
                 <label>Una pàgina web (500€)</label>
             </div>
+            <Panel @changeNumPages="updateNumPages($event)" @changeNumLanguages="updateNumLanguages($event)" />
             <div>
                 <input type="checkbox" :value="300" v-model="prices" @change="updateTotal" />
                 <label>Una consultoria SEO (300€)</label>
@@ -15,26 +16,39 @@
                 <label>Una campanya de Google Ads (200€)</label>
             </div>
         </form>
-        <p>Preu: {{ totalInitial }}€</p>
+        <p>Preu: {{ totalPrice }}€</p>
     </div>
 </template>
 
 <script>
+    import Panel from './Panel.vue';
+    
     export default {
         name: 'Home',
+        components: {
+            Panel
+        },
         data() {
             return {
                 prices: [],
-                totalInitial: 0
+                totalPrice: 0,
+                numPages: '',
+                numLanguages: ''
             }
         },
         methods: {
             updateTotal() {
-                this.totalInitial = 0;
+                this.totalPrice = 0;
                 this.prices.forEach((price) => {
-                    this.totalInitial += price;
+                    this.totalPrice += price;
                 });
-                console.log(this.prices)
+                console.log(this.numPages);
+            },
+            updateNumPages(updatedNumPages) {
+                this.numPages = updatedNumPages;
+            },
+            updateNumLanguages(updatedNumLanguages) {
+                this.numLanguages = updatedNumLanguages;
             }
         }
     }
---
+++ Nivel1/src/components/Panel.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <label>Nombre de pàgines </label>
+        <input type="text" v-model="numPages" @input="changeNumPages()" required/>
+    </div>
+    <div>
+        <label>Nombre d'idiomes </label>
+        <input type="text" v-model="numLanguages" @input="changeNumLanguages()" required/>
+    </div>
+    <p>{{ numPages }} {{ numLanguages}}</p>
+</template>
+
+<script>
+export default {
+    name: 'Panel',
+    emits: ['changeNumPages', 'changeNumLanguages'],
+    data() {
+        return {
+            numPages: '1',
+            numLanguages: '1'
+        }
+    },
+    methods: {
+        changeNumPages() {
+            this.$emit('changeNumPages', this.numPages);
+        },
+        changeNumLanguages() {
+            this.$emit('changeNumLanguages', this.numLanguages);
+        }
+    }
+}
+</script>
